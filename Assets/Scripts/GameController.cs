551617cc5eacd7c8a14861fe1221cff84ce602ed
rohitdesaidev <desairohit15@gmail.com>
2018-01-14 19:53:15 -0600

    repo added to remote

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	private int score=0;
+
+	public TextMesh scoreobj;
+	void Start ()
+	{
+		score = 0;
+		UpdateScore ();
+//		StartCoroutine (ResetCubes ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void AddScore (int newScoreValue)
+	{
+		score++;
+		UpdateScore ();
+	}
+
+
+	void UpdateScore ()
+	{
+		scoreobj.text = ""+score;
+	}
+
+	void ResetCubes ()
+	{
+		
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "ball") {
+			AddScore (score);
+			Debug.Log ("-----enter----" + score);
+		}
+	
+	}
+}
